bea13bc159d7a44bad7b84899c65b4098a461201
araxione <lml.judyy@gmail.com>
2021-10-23 15:26:39 -0700

    Nav bar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      <h1>Welcome</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+
+  {
+    title: "GRASP",
+    url: "https://ubcbionics.com/page2.html",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Teams",
+    url: "#",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Join Us",
+    url: "#",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Sponsors",
+    url: "#",
+    cName: "nav-links",
+  },
+];
