ffb4e3ac46b7fa3ccb10c68c9af05e3a32fee0a0
Amighty-font <timothyfang9@gmail.com>
2021-11-23 23:53:57 -0800

    fixed some spacings

--- src/Grasp.js
+++ src/Grasp.js
@@ -7,23 +7,25 @@ function Grasp() {
     return(
         <div className="Grasp">
             <div class="headingGrasp"> GRASP PROJECT</div>
+            <p className="graspBold">
+                What is Grasp?
+            </p>
 
             <p className="graspDesc">
                 GRASP is UBC Bionics' first attempt at the creation of a smart <br />
                 prosthetic arm. Our current design of the arm features a variety <br />
                 of functionalities including:
-        
             </p>
             <p className="graspDesc"> 
-                <ul>
-                    <li>Object recognition via an onboard camera </li>
+                <ul><li>Object recognition via an onboard camera </li>
                     <li>Various hand and finger grips </li>
                     <li>Integration with machine learning</li>
                     <li>Incorporation fo an ergonomic design using a shoulder sling <br />
                     and consolidate electrical components </li>
                     <li>EMG signalling</li>
                 </ul>
-            </p>    
+            </p>
+            <div class="headingGrasp"> FEATURES</div>
         </div>
 
     );
