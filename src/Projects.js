caa028fec155a4ddde1105e4e499ce7d354dcb50
araxione <lml.judyy@gmail.com>
2022-07-16 14:53:19 -0700

    updates

--- src/Projects.js
+++ src/Projects.js
@@ -15,15 +15,6 @@ export const Projects = () => {
   return (
     <Layout>
       <div class="py-4" />
-      {/* <div class="headerBlue text-center">OUR GOALS</div>
-      <div class="py-2" />
-      <p className="graspText">
-        Currently, we have undertaken two main projects; our pilot project,
-        GRASP, which is a smart bionic arm designed to enhance daily life for
-        amputees. Recently, we have also initiated the design of a
-        brain-computer interface, NERV. We hope to submit both of our projects
-        to the leading global bionics competition, CYBATHLON 2024.{" "}
-      </p> */}
       <div class="py-2" />
       <div class="headerBlue text-center">GRASP PROJECT</div>
       <div class="py-2" />
@@ -33,26 +24,12 @@ export const Projects = () => {
         iteration of the design. Our goal is to submit GRASP at the global
         bionics competition, CYBATHLON 2024.
       </p>
-      <div class="row">
-        {/* <div class="py-3" /> */}
-        {/* <div class="col">
-          <img
-            class="picOne img-responsive center-block d-block mx-auto"
-            src={pic1}
-          />
-        </div> */}
-        {/* <div class="col">
-          <img
-            class="picTwo img-responsive center-block d-block mx-auto"
-            src={pic2}
-          /> */}
-        {/* </div> */}
-      </div>
-      {/* <div class="py-3" /> */}
+      <div class="row"></div>
+
       <div class="row">
         <div class="col-sm col-order-2">
           <div class="py-5" />
-          {/* <p className="graspBold">What is GRASP?</p> */}
+
           <p className="graspDesc">
             Our current design features a variety of funtionalities including
             the following:
@@ -84,57 +61,17 @@ export const Projects = () => {
         </div>
 
         <div class="col-sm col-order-1">
-          {/* <div class="py-4" /> */}
           <img
             class="graspHand img-responsive center-block d-block mx-auto"
             src={bionicHand}
           />
         </div>
       </div>
+
       <div class="py-4" />
-      <div class="headerBlue text-center">FEATURES</div>
-      <div class="py-4" />
-      <div class="row">
-        <div class="col-sm-4">
-          <img
-            class="graspIcons img-responsive center-block d-block mx-auto"
-            src={softMaterials}
-          />
-          <p class="graspIcons text-center">
-            <p class="bold">Soft Materials</p>
-            The use of soft materials to create an anthropomorphic desing that
-            reduces social stigma and allows for more satisfying human-to-human
-            interaction.
-          </p>
-        </div>
-        <div class="col-sm-4">
-          <img
-            class="graspIcons img-responsive center-block d-block mx-auto"
-            src={nuanceControl}
-          />
-          <p class="graspIcons text-center">
-            <p class="bold">Nuanced Control</p>
-            The use of two levels of control, myoelectric and electromechanical,
-            to bridge the gap between patient preferences and technological
-            limitations.
-          </p>
-        </div>
-        <div class="col-sm-4">
-          <img
-            class="graspIcons img-responsive center-block d-block mx-auto"
-            src={costEffective}
-          />
-          <p class="graspIcons text-center">
-            <p class="bold">Cost-Effective</p>
-            The use of 3-D pritning to reduce costs and allow for rapid
-            manufacturing of the hand
-          </p>
-        </div>
-      </div>
       <div class="py-4" />
       <div class="headerBlue text-center">BCI PROJECT</div>
       <div class="py-2" />
-      <div class="py-4" />
       <p className="graspBold">What is NERV?</p>
       <p className="graspDesc">
         NERV is UBC Bionics' current design of a BCI, or brain computer
