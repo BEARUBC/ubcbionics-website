d66f122ac8fcd015f7dcd74ae83b03ad3a3040b6
leticiacid0478 <leticiacid0478@gmail.com>
2024-05-07 13:42:02 -0700

    Started on thank you slideshow on sponsors page

--- src/Sponsor.js
+++ src/Sponsor.js
@@ -17,6 +17,34 @@ import ubcmaterials from "./Photos/Bronze/ubcmaterials.jpg";
 export const Sponsor = () => {
   return (
     <Layout>
+      {/* <div class="py-2" />
+      <div className="headerBlue text-center">THANK YOU!</div>
+      <div class="py-3" />
+      
+      <div class="slideshow-container">
+        <div class="mySlides fade">
+          <img src="img1.jpg" style="width:100%"/>
+          <div class="text">Caption Text</div>
+        </div>
+
+        <div class="mySlides fade">
+          <img src="img2.jpg" style="width:100%"/>
+          <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+          <img src="img3.jpg" style="width:100%"/>
+          <div class="text">Caption Three</div>
+        </div>
+      </div>
+
+      {/*The dots/circles*
+      <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+      </div> */}
+
       <div class="py-4" />
       <div className="headerBlue text-center">OUR SPONSORS</div>
       <div class="py-3" />
